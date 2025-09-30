e8e1054244348ee6877e986996ab1e1055758246
Jake <228561383+jakelewy@users.noreply.github.com>
2025-09-30 08:16:58 -0700

    add main structure

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+class gameState {
+    private:
+        float points;
+        std::vector<const char*> packsInInventory;
+    public:
+        gameState() = default;
+        ~gameState() = default;
+
+    // todo: add get and set methods
+};
+
+int main() {
+    // todo: start game stuff and init settings
+
+
+    while (true) {
+        // todo: show shop and inv.
+
+        // todo: open packs and flip coins
+        
+    }
+}
